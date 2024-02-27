045741a37f4bbb1f8e1037d24430ab7ac4a7b41d
Jacob Swiniarski <36275900+JsPako@users.noreply.github.com>
2024-02-27 13:39:57 +0000

    Added Confidence to kNN.py

--- kNN.py
+++ kNN.py
@@ -4,6 +4,7 @@ from collections import Counter
 
 
 class kNN:
+
     # kNN Classifier Constructor
     # To initialise, the number of neighbours the model will use needs to be provided.
     def __init__(self, number_of_neighbours):
@@ -19,40 +20,33 @@ class kNN:
         self.trainLabels = training_labels
 
     # Predict function that takes the testing data,
-    # and iterates through the list passing each row to the prediction algorithm,
-    # returns a list of predictions.
-    def predict(self, testing_data):
+    # and iterates through the list passing each value to the prediction algorithm,
+    # returns a list of predictions, and the confidence if set to True.
+    def predict(self, testing_data, confidence = False):
+        if len(testing_data) == 0:
+            return -1
+
         for test_value in testing_data:
-            self.predictionResults.append(self._predict(test_value))
+            self.predictionResults.append(self._predict(test_value, confidence))
         return self.predictionResults
 
-    # kNN Prediction Algorithm,
-    # takes 1 testing data row with the same number of features as training data,
-    # returns predicted label.
-    def _predict(self, test_value):
+    def _predict(self, test_value, confidence):
         distances = []
-
-        # For each row in the training data,
-        # do the Euclidean distance equation with the testing row passed,
-        # add the results to the distances list.
         for train_value in self.trainData:
             euclidean = 0
             for feature in range(train_value.shape[0]):
                 euclidean += (test_value[feature] - train_value[feature]) ** 2
             distances.append(math.sqrt(euclidean))
-
-        # Convert the distances list to a pandas dataframe
         distances = pd.DataFrame({'distance': distances})
-        # Match each row of the calculated distances to the correct training labels
         distances["labels"] = self.trainLabels
-        # Sort the calculated distance values from lowest to largest
         distances = distances.sort_values('distance')
-        # Keep only the k number of lowest distance values
         distances = distances.iloc[:self.k]
-        # Convert the leftover labels column to a python list
         distances = distances['labels'].tolist()
-        # Get the most common label in the list and return it as the predicted value
         most_common = Counter(distances).most_common(1)
+
+        # Returns either 1D or 2D array
+        if confidence == True:
+          return most_common[0][0], (most_common[0][1] / self.k)
         return most_common[0][0]
 
     # Simple function to quick return the accuracy of the model as a decimal.
@@ -61,4 +55,5 @@ class kNN:
         # if it is empty then return -1.
         if not self.predictionResults or not testing_labels.any():
             return -1
+            
         return (sum(self.predictionResults == testing_labels)) / len(testing_labels)
