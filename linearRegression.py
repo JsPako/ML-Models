48bbd31116420c25cddf182046e98dd2c3399679
jspako <jacobswiniarski@gmail.com>
2024-03-12 18:07:37 +0000

    Changed Model to use Gradient Descent

--- linearRegression.py
+++ linearRegression.py
@@ -10,76 +10,58 @@ class LinearRegressor:
 
     # Fit function that takes the training data and the training target values,
     # and sets them as variable instances inside the class.
-    def fit(self, training_data, training_values):
+    def fit(self, training_data, training_values, epoch=5000, alpha=0.01):
         self.trainData = training_data
         self.trainValues = training_values
 
-        # Variables needed to perform the least squares calculation.
-        sums_of_x = []
-        sums_of_x2 = []
-        sum_of_y = 0
-        sums_of_xy = []
-
-        # Iterate through the dependent variable,
-        # and set the sum of all the values as 'sum_of_y'.
-        for value in self.trainValues:
-            sum_of_y += value
-
-        # Iterate through each feature(independent variables),
-        # and sum all the x values, x^2 values, and x * y values,
-        # save each feature values separately by appending the summed values to an appropriate array.
         for feature in range(self.trainData.shape[1]):
-            sum_x = 0
-            sum_x2 = 0
-            sum_xy = 0
-            for x, y in zip(self.trainData, self.trainValues):
-                sum_x += x[feature]
-                sum_x2 += x[feature] ** 2
-                sum_xy += x[feature] * y
+            gradient = 0
+            intercept = 0
 
-            sums_of_x.append(sum_x)
-            sums_of_x2.append(sum_x2)
-            sums_of_xy.append(sum_xy)
+            for iteration in range(epoch):
+                dl_gradient = 0
+                dl_intercept = 0
 
-        # Iterate through the prepared arrays,
-        # and calculate a coefficient for each feature using the least squares method,
-        # then use the coefficient to calculate the intercept for each feature.
-        for feature in range(self.trainData.shape[1]):
-            n = self.trainData.shape[0]
-            self.coefficients.append(
-                ((n * sums_of_xy[feature]) - (sums_of_x[feature] * sum_of_y))
-                / ((n * sums_of_x2) - (sums_of_x2[feature])))
+                for x, y in zip(self.trainData, self.trainValues):
+                    dl_gradient += -2 * x[feature] * (y - (gradient * x[feature] + intercept))
+                    dl_intercept += -2 * (y - (gradient * x[feature] + intercept))
 
-            self.intercepts.append(sum_of_y - (self.coefficients[feature] * sums_of_x[feature]) / n)
+                gradient = gradient - (1 / len(self.trainData)) * dl_gradient * alpha
+                intercept = intercept - (1 / len(self.trainData)) * dl_intercept * alpha
 
-    # Iterate through the provided testing values,
-    # for each feature in the row calculate the target value using the appropriate line of best fit,
-    # sum the predicted target values,
-    # average the final sum,
-    # and return the average as the final prediction.
-    def predict(self, testing_values):
-        for row in testing_values:
-            target_value = 0
-            for feature in range(testing_values.shape[1]):
-                target_value += self.coefficients[feature] * row[feature] + self.intercepts[feature]
+            self.coefficients.append(gradient)
+            self.intercepts.append(intercept)
 
-            self.predictionResults.append(target_value / testing_values.shape[1])
 
-        return self.predictionResults
+# Iterate through the provided testing values,
+# for each feature in the row calculate the target value using the appropriate line of best fit,
+# sum the predicted target values,
+# average the final sum,
+# and return the average as the final prediction.
+def predict(self, testing_values):
+    for row in testing_values:
+        target_value = 0
+        for feature in range(testing_values.shape[1]):
+            target_value += self.coefficients[feature] * row[feature] + self.intercepts[feature]
 
-    # Simple function to quick return the mean squared error of the model.
-    def mean_squared_error(self, testing_values):
-        # Check to see if the predictions list or the testing values list is empty,
-        # if it is empty then return -1.
-        if not self.predictionResults or not testing_values.any():
-            return -1
+        self.predictionResults.append(target_value / testing_values.shape[1])
 
-        # Try to calculate mean squared error from the predicted results and provided testing values,
-        # then return the mean squared error.
-        total = 0
-        for predicted, true in zip(self.predictionResults, self.trainValues):
-            total += (predicted - true) ** 2
+    return self.predictionResults
 
-        mse = total / len(testing_values)
 
-        return mse
+# Simple function to quick return the mean squared error of the model.
+def mean_squared_error(self, testing_values):
+    # Check to see if the predictions list or the testing values list is empty,
+    # if it is empty then return -1.
+    if not self.predictionResults or not testing_values.any():
+        return -1
+
+    # Try to calculate mean squared error from the predicted results and provided testing values,
+    # then return the mean squared error.
+    total = 0
+    for predicted, true in zip(self.predictionResults, self.trainValues):
+        total += (predicted - true) ** 2
+
+    mse = total / len(testing_values)
+
+    return mse
