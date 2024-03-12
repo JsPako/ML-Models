d2bfe69ce765264060c2b9581a6ed01ce217953f
jspako <jacobswiniarski@gmail.com>
2024-03-12 14:37:06 +0000

    Non Tested Commit

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,5 +1,5 @@
-from collections import Counter
 import math
+from collections import Counter
 
 
 class NaiveBayes:
@@ -9,7 +9,7 @@ class NaiveBayes:
         self.trainData = None
         self.trainLabels = None
         self.classProbability = {}
-        self.featureProbability = {}
+        self.featureMeanStandardDeviation = {}
         self.predictionResults = []
 
     # Fit function that takes the training data and the training labels,
@@ -40,9 +40,8 @@ class NaiveBayes:
             data_dictionary[class_name].append(data)
 
         # Set up the data dictionary that will be used to store the mean and standard deviation
-        mean_std = {}
         for class_name in unique_classes:
-            mean_std[class_name] = []
+            self.featureMeanStandardDeviation[class_name] = []
 
         # Calculate the mean and standard deviation for each feature,
         # Each class should have N feature number of means and standard deviations,
@@ -51,14 +50,64 @@ class NaiveBayes:
         for data in data_dictionary.values():
             for feature in range(len(data[0])):
                 mean = 0
-                std = 0
                 for row in data:
                     mean += row[feature]
-                    std += row[feature]
 
                 mean = mean / len(data)
-                std = math.sqrt((std - mean) ** 2 / len(data))
 
-                mean_std[unique_classes[index]].append([mean, std])
+                std = 0
+                for row in data:
+                    std += (row[feature] - mean) ** 2
+
+                std = math.sqrt(std / len(data))
+
+                self.featureMeanStandardDeviation[unique_classes[index]].append([mean, std])
 
             index += 1
+
+    # Predict function that takes the testing data,
+    # and iterates through the testing data,
+    # and for each row calculates the likelihood that it belongs in each class,
+    # gets the class label that has the highest likelihood and saves that as the prediction result,
+    # returns a list of predictions, and the confidence if set to True.
+    def predict(self, testing_data, confidence=False):
+        for row in testing_data:
+            predictions = {}
+
+            for class_name, class_probability in self.classProbability.items():
+                predictions[class_name] = 1
+                for feature, (mean, std) in enumerate(self.featureMeanStandardDeviation[class_name]):
+                    predictions[class_name] *= self.probability_density_function(row[feature], mean, std)
+                predictions[class_name] *= class_probability
+
+            prediction = max(predictions, key=predictions.get)
+            if confidence:
+                prediction = list(max(predictions.items(), key=lambda item: item[1]))
+            self.predictionResults.append(prediction)
+
+        return self.predictionResults
+
+    # Calculate the likelihood that a provided value is within the normal distribution.
+    @staticmethod
+    def probability_density_function(value, mean, std):
+        exponent = math.exp(- (value - mean) ** 2 / (2 * (std ** 2)))
+        base = 1 / math.sqrt(2 * math.pi * (std ** 2))
+        return base * exponent
+
+    # Simple function to quick return the accuracy of the model as a decimal.
+    def accuracy(self, testing_labels):
+        # Check to see if the predictions list or the testing labels list is empty,
+        # if it is empty then return -1.
+        if not self.predictionResults or not testing_labels.any():
+            return -1
+
+        # Try to calculate accuracy from the predicted results and provided testing labels,
+        # and if the prediction results also contain the confidence values,
+        # catch the value error (comparing 2D to 1D list),
+        # then extract the labels and save as a new accuracy list,
+        # calculate accuracy using new list and return accuracy value.
+        try:
+            return (sum(self.predictionResults == testing_labels)) / len(testing_labels)
+        except ValueError:
+            accuracy_list = [row[0] for row in self.predictionResults]
+            return (sum(accuracy_list == testing_labels)) / len(testing_labels)
