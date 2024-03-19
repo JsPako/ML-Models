d2473eaec9b5f2dea5144854976111cd6d6e7059
jspako <jacobswiniarski@gmail.com>
2024-03-19 16:54:46 +0000

    SVM Initial Commit

--- supportVectorMachine.py
+++ supportVectorMachine.py
@@ -1,3 +1,33 @@
 class SupportVectorMachine:
     def __init__(self):
+        self.trainData = None
+        self.trainLabels = None
+        self.coefficients = []
+        self.intercepts = []
+        self.predictionResults = []
+
+    def fit(self):
         pass
+
+    def predict(self):
+        pass
+
+    def accuracy(self, testing_labels):
+
+        #   Accuracy Function
+
+        #   This function is needed to evaluate the performance of the KNN algorithm, provided the testing classes this
+        #   function checks how many of the predicted classes match the testing classes. Returning the decimal value of
+        #   how many were correct. Based on this accuracy information you can tell if the model needs more optimisation
+        #   by adjusting the number of nearest neighbours considered.
+
+        if not self.predictionResults or not testing_labels.any():
+            return -1
+
+        try:
+            return (sum(self.predictionResults == testing_labels)) / len(testing_labels)
+        except ValueError:
+            #   Makes an accuracy list without the confidence values before trying the comparison again,
+            #   it turns the 2D == 1D into 1D == 1D which Python can do.
+            accuracy_list = [row[0] for row in self.predictionResults]
+            return (sum(accuracy_list == testing_labels)) / len(testing_labels)
