92b0882fd9bfffedef8085ef1c47f7464bbfbd2b
jspako <jacobswiniarski@gmail.com>
2024-03-25 16:46:02 +0000

    Predict Feature Added & Fixed Label Encoding Error

--- supportVectorMachine.py
+++ supportVectorMachine.py
@@ -1,5 +1,5 @@
 from itertools import combinations
-
+from collections import Counter
 import numpy as np
 
 
@@ -7,6 +7,8 @@ class SupportVectorMachine:
     def __init__(self):
         self.trainData = None
         self.trainLabels = None
+        self.unique_classes = []
+        self.pairings = []
         self.coefficients = []
         self.intercepts = []
         self.predictionResults = []
@@ -15,20 +17,20 @@ class SupportVectorMachine:
         self.trainData = training_data
         self.trainLabels = training_labels
 
-        unique_classes = list(set(self.trainLabels))
+        self.unique_classes = list(set(self.trainLabels))
 
-        pairings = list(combinations(unique_classes, 2))
+        self.pairings = list(combinations(self.unique_classes, 2))
 
         data_pairing = []
-        for pairing in pairings:
+        for pairing in self.pairings:
             value_pairing = []
             for data in zip(self.trainData, self.trainLabels):
                 if data[1] in pairing:
                     data = list(data)
                     if data[1] == pairing[0]:
-                        data[1] = -1
-                    else:
                         data[1] = 1
+                    else:
+                        data[1] = -1
                     value_pairing.append(data)
             data_pairing.append(value_pairing)
 
@@ -49,8 +51,24 @@ class SupportVectorMachine:
         self.coefficients.append(gradient)
         self.intercepts.append(intercept)
 
-    def predict(self):
-        pass
+    def predict(self, testing_data, confidence=False):
+        for sample in testing_data:
+            class_prediction = []
+            for index, pairing in enumerate(self.pairings):
+                margin = np.dot(sample, self.coefficients[index]) - self.intercepts[index]
+                if margin > 0:
+                    class_prediction.append(pairing[0])
+                else:
+                    class_prediction.append(pairing[1])
+
+            most_common = Counter(class_prediction).most_common(1)
+            if confidence:
+                confidence_value = most_common[0][1] / len(self.unique_classes)
+                self.predictionResults.append([most_common[0][0], confidence_value])
+
+            self.predictionResults.append(most_common[0][0])
+
+        return self.predictionResults
 
     def accuracy(self, testing_labels):
 
